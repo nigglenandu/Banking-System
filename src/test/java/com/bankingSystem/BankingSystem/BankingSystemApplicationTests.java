e95752943d4e06ee6d253b51e64ff86544d7f3ad
[Niggle Nandu] <nigglenandu@gmail.com>
2025-01-30 23:36:23 +0545

    project added

---
+++ src/test/java/com/bankingSystem/BankingSystem/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankingSystem.BankingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
