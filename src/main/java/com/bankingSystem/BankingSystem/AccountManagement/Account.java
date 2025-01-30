b52e453141603b7600f9b6e15e2e4f42ffd6ccaf
[Niggle Nandu] <nigglenandu@gmail.com>
2025-01-30 23:44:21 +0545

    account and user entity

---
+++ src/main/java/com/bankingSystem/BankingSystem/AccountManagement/Account.java
@@ -0,0 +1,49 @@
+package com.bankingSystem.BankingSystem.AccountManagement;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long UserId;
+    private String accountType;
+    private Double balance;
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(Long userId) {
+        UserId = userId;
+    }
+}
