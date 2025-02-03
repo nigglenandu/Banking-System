8cb23319d05d70ca5e0e33c89e57f05d75a28375
[Niggle Nandu] <nigglenandu@gmail.com>
2025-02-04 00:06:50 +0545

    transaction controller error solved

--- src/main/java/com/bankingSystem/BankingSystem/TransactionManagement/TransactionController.java
+++ src/main/java/com/bankingSystem/BankingSystem/TransactionManagement/TransactionController.java
@@ -1,2 +1,39 @@
-package com.bankingSystem.BankingSystem.TransactionManagement;public class TransactionController {
+package com.bankingSystem.BankingSystem.TransactionManagement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/transactions")
+public class TransactionController {
+    @Autowired
+    private IServiceTransaction serviceTransaction;
+
+    @PostMapping("/create")
+    public ResponseEntity<Transaction> createTransaction(@RequestBody Transaction transaction){
+        return new ResponseEntity<>(serviceTransaction.save(transaction), HttpStatus.OK);
+    }
+
+    @GetMapping("/account/{accountId}")
+    public ResponseEntity<List<Transaction>> getTransactionByAccountId(@PathVariable Long accountId){
+        return  serviceTransaction.getTransactionByAccountId(accountId)
+                .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Transaction>> getAllTransactions(){
+        return new ResponseEntity<>(serviceTransaction.getAllTransactions(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Transaction> getTransactionById(@PathVariable Long id){
+        return  serviceTransaction.getTransactionById(id)
+                .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
+    }
 }
