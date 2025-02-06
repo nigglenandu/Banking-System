a12494e7eb84dd02568060efaf8df894394e59c0
[Niggle Nandu] <nigglenandu@gmail.com>
2025-02-06 23:31:55 +0545

    jwt security dependency

--- src/main/java/com/bankingSystem/BankingSystem/TransactionManagement/Transaction.java
+++ src/main/java/com/bankingSystem/BankingSystem/TransactionManagement/Transaction.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 import java.util.Date;
 
@@ -12,9 +14,12 @@ public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
     private Long accountId;
+    @Positive
     private Double amount;
     private Date date;
+    @NotNull
     private String transactionType;
 
     public Long getAccountId() {
