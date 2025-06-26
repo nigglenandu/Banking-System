46005bfa94b7e426e274e8a3e1bf96c4d92dfb20
[Niggle Nandu] <nigglenandu@gmail.com>
2025-06-26 23:42:50 +0545

    optional error

--- src/main/java/com/bankingSystem/BankingSystem/User/AppUserRepository.java
+++ src/main/java/com/bankingSystem/BankingSystem/User/AppUserRepository.java
@@ -3,8 +3,10 @@ package com.bankingSystem.BankingSystem.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 
-    AppUser findByUsername(String username);
+    Optional<AppUser> findByUsername(String username);
 }
